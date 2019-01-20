9cf17061ce8a0c1046c4d5e818da933dc60409fd
trunglq996 <46856894+trunglq996@users.noreply.github.com>
2019-01-20 11:31:23 +0700

    trunglq996
    first commit

---
+++ smss/DangNhap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using smss.classKN;
+
+namespace smss
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDN_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
