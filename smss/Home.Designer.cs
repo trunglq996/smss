afbdf0f62329545d40ac3b258f4467d040e0a3ee
trunglq996 <46856894+trunglq996@users.noreply.github.com>
2019-02-15 10:38:47 +0700

    trung

--- smss/Home.Designer.cs
+++ smss/Home.Designer.cs
@@ -28,61 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.btnLogo = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.btnLogo);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(862, 100);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Location = new System.Drawing.Point(0, 106);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(153, 323);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // btnLogo
-            // 
-            this.btnLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLogo.BackgroundImage")));
-            this.btnLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnLogo.Location = new System.Drawing.Point(38, 0);
-            this.btnLogo.Name = "btnLogo";
-            this.btnLogo.Size = new System.Drawing.Size(103, 88);
-            this.btnLogo.TabIndex = 2;
-            this.btnLogo.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1170, 514);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(780, 334);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.Text = "Quản lý sinh viên";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button btnLogo;
     }
 }
 
--- smss/Home.cs
+++ smss/Home.cs
@@ -7,13 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using smss.classKN;
 
 namespace smss
 {
     public partial class Home : Form
     {
-        control.WelcomeHome wc = new control.WelcomeHome();
+        readonly control.WelcomeHome _wc = new control.WelcomeHome();
         public Home()
         {
             InitializeComponent();
@@ -22,17 +21,11 @@ namespace smss
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            panel1.Width = 1000;
-            panel1.Height = 80;
-            panel2.Height = 520;
-            panel2.Width = 150;
-            btnLogo.Width = 80;
-            btnLogo.Height = 80;
             Width = 1000;
             Height = 600;
-            Controls.Add(wc);
-            wc.Location = new Point(400,200);
-            wc.Show();
+            Controls.Add(_wc);
+            _wc.Location = new Point(400,200);
+            _wc.Show();
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
