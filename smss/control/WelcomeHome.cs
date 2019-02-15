8eb7cd4a0395d265396aa6c9bb4618fe3ce30841
trunglq996 <46856894+trunglq996@users.noreply.github.com>
2019-02-15 11:12:15 +0700

    Update WelcomeHome.cs

--- smss/control/WelcomeHome.cs
+++ smss/control/WelcomeHome.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using smss.Connection;
 
 namespace smss.control
 {
@@ -25,7 +26,8 @@ namespace smss.control
 
         private void btnDN_Click(object sender, EventArgs e)
         {
-
+            DataSet ds = new DataSet();
+    
         }
     }
 }
