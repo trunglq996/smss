3ce9fa839a771ec79ec1af1edceb203f94b4794f
trunglq996 <46856894+trunglq996@users.noreply.github.com>
2019-02-15 11:09:28 +0700

    trung

--- smss/Connection/Connection.cs
+++ smss/Connection/Connection.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +11,64 @@ namespace smss.Connection
 {
     class Connection
     {
+        private readonly string _conStr;
+        public readonly int Status;
+        SqlConnection _connection;
+
         public Connection()
         {
+            _conStr =
+                "Data Source=Trung;Initial Catalog=otest;integrated security=true; Max Pool Size=1024; ";
+            try
+            {
+                _connection = new SqlConnection(_conStr);
+                _connection.Open();
+            }
+            catch (Exception)
+            {
+                Status = -1000;
+            }
+        }
+        public int DoSql(string sql)
+        {
+            int ret = Status;
+            if (ret >= 0)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(sql, _connection) { CommandType = CommandType.Text };
+                    ret = cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
 
+                    ret = -1;
+                }
+            }
+            _connection.Close();
+            return ret;
+        }
+
+        public int GetDataByQuery(ref DataSet ds, string tableName, string sql)
+        {
+            ds.Tables.Add(tableName);
+            int ret = Status;
+            if (ret >= 0)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(sql, _connection) { CommandType = CommandType.Text };
+                    SqlDataAdapter data = new SqlDataAdapter(cmd);
+                    data.Fill(ds.Tables[tableName]);
+                }
+                catch (Exception)
+                {
+
+                    ret = -1;
+                }
+            }
+            _connection.Close();
+            return ret;
         }
     }
 }
